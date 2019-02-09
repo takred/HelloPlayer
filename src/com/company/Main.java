71fb80cdeb4ca020e1b5a76957118301db92c371
takred <sazonov.kos@yandex.ru>
2019-02-09 23:59:20 +0300

    Оптимизирован код.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,17 +1,16 @@
 package com.company;
 
 public class Main {
-    static String Hello(int lastLog, int currentLog){
-        int dayLastLog = (((currentLog - lastLog) / 60) / 60)/ 24;
+    static String hello(int lastLog, int currentLog){
+        int dayLastLog = (currentLog - lastLog) / 86400;
         if (dayLastLog >= 7){
             return "Здравствуйте! Вы отсутствовали больше недели - НЕ НАДО ТАК!";
         }
         return "Проходи, не задерживайся.";
     }
-    static String HelloTimesOfDay(int currentLog){
-        int fullDay  = ((currentLog / 60) / 60)/ 24;
-        int daySec = fullDay * 24 * 60 * 60;
-        int dayTime = ((currentLog - daySec) / 60) / 60;
+    static String helloTimesOfDay(int currentLog){
+        int dayTime = (currentLog % 86400) / 3600;
+        System.out.println(dayTime);
         if (dayTime < 6){
             return "Доброй ночи.";
         } else if (dayTime < 12){
@@ -23,9 +22,9 @@ public class Main {
         }
         return "Что-то фигня какая-то";
     }
-    static String OneHelloInDay (int lastLog, int currentLog){
-        int lastLogDay = lastLog / 60 / 60 / 24;
-        int currentLogDay = currentLog / 60 / 60 / 24;
+    static String oneHelloInDay(int lastLog, int currentLog){
+        int lastLogDay = lastLog / 86400;
+        int currentLogDay = currentLog / 86400;
         if (lastLogDay != currentLogDay){
             return "Ну здорова!";
         }
@@ -35,11 +34,11 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         int lastLog = 100;
-        int currentLog = 439045;
-        System.out.println(Hello(lastLog, currentLog));
-        System.out.println(HelloTimesOfDay(currentLog));
+        int currentLog = 379045;
+        System.out.println(hello(lastLog, currentLog));
+        System.out.println(helloTimesOfDay(currentLog));
         for (int i = 0; i < 10; i++){
-            System.out.println(OneHelloInDay(lastLog, currentLog));
+            System.out.println(oneHelloInDay(lastLog, currentLog));
             lastLog = currentLog;
             currentLog = currentLog + 10000;
         }
