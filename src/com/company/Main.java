2a43550cf618036923f658817ea7994b056783eb
takred <sazonov.kos@yandex.ru>
2019-02-10 20:09:47 +0300

    Сделан 4-ый метод в Main объединяющий весь функционал предыдущих методов.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,35 +1,58 @@
 package com.company;
 
 public class Main {
-    static String hello(int lastLog, int currentLog){
+    static String hello(int lastLog, int currentLog) {
         Interval currentLogInter = new Interval(currentLog);
         Interval lastLogInter = new Interval(lastLog);
-        if (currentLogInter.subtract(lastLogInter).days() >= 7){
+        if (currentLogInter.subtract(lastLogInter).days() >= 7) {
             return "Здравствуйте! Вы отсутствовали больше недели - НЕ НАДО ТАК!";
         }
-        return helloTimesOfDay(currentLog);
+        return "Проходи, не задерживайся.";
     }
-    static String helloTimesOfDay(int currentLogSec){
+
+    static String helloTimesOfDay(int currentLogSec) {
         Interval currentLogInter = new Interval(currentLogSec);
         Interval fullDayInSec = Interval.fromDays(currentLogInter.days());
         Interval dayTime = currentLogInter.subtract(fullDayInSec);
-        System.out.println(dayTime);
-        if (dayTime.hours() < 6){
+        if (dayTime.hours() < 6) {
             return "Доброй ночи.";
-        } else if (dayTime.hours() < 12){
+        } else if (dayTime.hours() < 12) {
             return "Доброе утро.";
-        } else if (dayTime.hours() < 18){
+        } else if (dayTime.hours() < 18) {
             return "Добрый день.";
-        } else if (dayTime.hours() < 24){
+        } else if (dayTime.hours() < 24) {
             return "Добрый вечер.";
         }
         return "Что-то фигня какая-то";
     }
-    static String oneHelloInDay(int lastLog, int currentLog){
+
+    static String oneHelloInDay(int lastLog, int currentLog) {
         Interval lastLogDay = new Interval(lastLog);
         Interval currentLogDay = new Interval(currentLog);
-        if (lastLogDay.days() != currentLogDay.days()){
-            return hello(lastLog, currentLog);
+        if (lastLogDay.days() != currentLogDay.days()) {
+            return "Ну здорова!";
+        }
+        return "";
+    }
+
+    static String helloPlayer(int lastLog, int currentLog) {
+        Interval lastLogInter = new Interval(lastLog);
+        Interval currentLogEnter = new Interval(currentLog);
+        if (currentLogEnter.days() - lastLogInter.days() >= 7) {
+            return "Здравствуйте! Вы отсутствовали больше недели - НЕ НАДО ТАК!";
+        }
+        if (lastLogInter.days() != currentLogEnter.days()) {
+            Interval fullDaysInSec = Interval.fromDays(currentLogEnter.days());
+            Interval dayTime = currentLogEnter.subtract(fullDaysInSec);
+            if (dayTime.hours() < 6) {
+                return "Доброй ночи.";
+            } else if (dayTime.hours() < 12) {
+                return "Доброе утро.";
+            } else if (dayTime.hours() < 18) {
+                return "Добрый день.";
+            } else if (dayTime.hours() < 24) {
+                return "Добрый вечер.";
+            }
         }
         return "";
     }
@@ -41,7 +64,8 @@ public class Main {
 //        System.out.println(hello(lastLog, currentLog));
 //        System.out.println(helloTimesOfDay(currentLog));
         for (int i = 0; i < 10; i++){
-            System.out.println(oneHelloInDay(lastLog, currentLog));
+            System.out.println(helloPlayer(lastLog, currentLog));
+//            System.out.println(oneHelloInDay(lastLog, currentLog));
             lastLog = currentLog;
             currentLog = currentLog + 10000;
         }
