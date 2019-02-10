948ac5652ef0d2c7aca9150cf4495da615501fa7
takred <sazonov.kos@yandex.ru>
2019-02-10 16:02:50 +0300

    Добавлен новый класс.

---
+++ src/com/company/Interval.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Interval {
+    int second;
+    public Interval(int second){
+        this.second = second;
+    }
+    public static int secInSec(){
+        return ;
+    }
+    public static int secInMin(){
+        return ;
+    }
+    public static int secInHour(){
+        return  ;
+    }
+}
